a1087b90a68bf947bbf3302ded8e818e01f5e107
viewfinder0 <97415261+viewfinder0@users.noreply.github.com>
2022-02-22 22:02:57 -0500

    Update theme.lua

--- theme.lua
+++ theme.lua
@@ -1,5 +1,5 @@
 ---------------------------
--- Default awesome theme --
+--    awesome   theme    --
 ---------------------------
 
 local theme_assets = require("beautiful.theme_assets")
@@ -12,25 +12,25 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font           = "sans 10"
+theme.font           = "source code pro 11"
 
-theme.bg_normal      = "#222222"		--default #222222
-theme.bg_focus       = "#222222"		--default #535d6c
-theme.bg_urgent      = "#ff0000"		--default #ff0000
-theme.bg_minimize    = "#444444"		--default #aaaaaa
+theme.bg_normal      = "#282828"    
+theme.bg_focus       = "#282828"   
+theme.bg_urgent      = "#282828"
+theme.bg_minimize    = "#282828"  
 theme.bg_systray     = theme.bg_normal
 
-theme.fg_normal      = "#535d6c"		--default #aaaaaa
-theme.fg_focus       = "#aaaaaa"		--default #ffffff
-theme.fg_urgent      = "#ffffff"		--default #ffffff
-theme.fg_minimize    = "#ffffff"		--default #ffffff
-theme.mytextclock = theme.fg_focus
+theme.fg_normal      = "#a89984"
+theme.fg_focus       = "#ebdbb2"
+theme.fg_urgent      = "#fb4934"
+theme.fg_minimize    = "#928374"
+theme.mytextclock    = theme.fg_focus
 
 theme.useless_gap    = dpi(4)
-theme.border_width   = dpi(4)
-theme.border_normal  = "#535d6c"		--default #000000
-theme.border_focus   = "#aaaaaa"		--default #535d6c
-theme.border_marked  = "#91231c"		--default #91231c
+theme.border_width   = dpi(2)
+theme.border_normal  = "#282828"
+theme.border_focus   = "#ebdbb2"      
+theme.border_marked  = "#fb4934"
 
 -- There are other variable sets
 -- overriding the default one when
@@ -53,6 +53,8 @@ theme.taglist_squares_sel = theme_assets.taglist_squares_sel(
 theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
     taglist_square_size, theme.fg_normal
 )
+--theme.taglist_spacing     = 2
+--theme.taglist_font        = "awesomewm-font 14"
 
 -- Variables set for theming notifications:
 -- notification_font
@@ -99,7 +101,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/floating_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
 
-theme.wallpaper = themes_path.."default/background.png"
+theme.wallpaper = "~/.wall.jpg" 
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
@@ -126,7 +128,7 @@ theme.awesome_icon = theme_assets.awesome_icon(
 
 -- Define the icon theme for application icons. If not set then the icons
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
-theme.icon_theme = nil --"Adwaita"
+theme.icon_theme = Paper   --"Adwaita"
 
 return theme
 
